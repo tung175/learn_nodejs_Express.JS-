dc1bda2379c2fafc83c610ea3e17d519dac414a6
tung175 <duongkq3@gmail.com>
2023-04-05 17:21:59 +0700

    final nodejs Express basic

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,7 @@
+import render  from "ejs";
 import pool from "../configs/connectDB";
-
+import multer from "multer";
+import path from 'path'
 let getHomepage = async (req, res) => {
     //logic
     let data = [];
@@ -75,6 +77,52 @@ let updateUser = async (req, res) =>{
     
 
 }
+
+let getUploadFile = async (req, res) =>{
+    return res.render('uploadFile.ejs')
+}
+
+let handleUploadFile = async (req, res) =>{
+    
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+   
+}
+
+let handleUploadMutipleFiless = async (req, res) =>{
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.files) {
+            return res.send('Please select an image to upload');
+        }
+    
+        let result = "You have uploaded these images: <hr />";
+        const files = req.files;
+        let index, len;
+
+        // Loop through all the uploaded images and display them on frontend
+        for (index = 0, len = files.length; index < len; ++index) {
+            result += `<img src="/image/${files[index].filename}" width="300" style="margin-right: 20px;">`;
+        }
+        result += '<hr/><a href="/upload">Upload more images</a>';
+        res.send(result);
+    
+}
+
 module.exports = {
-    getHomepage, getDetailPage, createNewUser, deleteUser, editUser, updateUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, editUser, updateUser ,getUploadFile, handleUploadFile, 
+    handleUploadMutipleFiless
+
 }
--- src/server.js
+++ src/server.js
@@ -2,16 +2,25 @@ import express  from 'express';
 import configsViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web'
 import initAPIRouter from './route/api'
+import { render } from 'ejs';
 
 // import connection from './configs/connectDB'
 require('dotenv').config()
+var morgan = require('morgan')
+
 
 const app = express()
 const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-
+app.use((req, res, next) =>{
+  //check => return res.send()
+  console.log('>>> run into my middleware')
+  console.log(req.method)
+  next(); 
+})
+app.use(morgan('combined'))
 //setup engine
 configsViewEngine(app)
 
@@ -21,6 +30,9 @@ initWebRoute(app)
 //inti api router
 initAPIRouter(app)
 
+app.use((req, res) =>{
+  return res.render('404.ejs')
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
